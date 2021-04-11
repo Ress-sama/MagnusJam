c78ecbbd4315e078c247ce414e1a719cc5b4b373
Ress <hacimuradoff.01@gmail.com>
2021-04-11 04:22:59 +0400

    Gadgets complete

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,11 @@ public class GameManager : MonoBehaviour
     GameObject leftButton;
     GameObject rightButton;
     GameObject player;
+    GameObject mouse;
+    GameObject toolBar;
+    GameObject ready;
+
+
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag(Tags.Player);
@@ -16,14 +21,27 @@ public class GameManager : MonoBehaviour
     {
         leftButton = GameObject.Find("Left");
         rightButton = GameObject.Find("Right");
+        mouse = GameObject.Find(StaticFields.Mouse);
+        toolBar = GameObject.Find(StaticFields.ToolBar);
+        ready = GameObject.Find(StaticFields.Ready);
+
         if (CheckButtons())
         {
             player.GetComponent<Player>().enabled = true;
         }
+
+        Vector3 targetMouse = new Vector3(mouse.transform.position.x + 800, mouse.transform.position.y);
+        Vector3 targetToolBar = new Vector3(toolBar.transform.position.x - 300, toolBar.transform.position.y);
+        Vector3 targetReady = new Vector3(ready.transform.position.x - 1000, ready.transform.position.y);
+
+        ready.transform.position = targetReady;
+        iTween.MoveTo(mouse, iTween.Hash("position", targetMouse, "time", 1.4f, "easeType", iTween.EaseType.easeInBack));
+        iTween.MoveTo(toolBar, iTween.Hash("position", targetToolBar, "time", 1.4f, "easeType", iTween.EaseType.easeInBack));
+
     }
     bool CheckButtons()
     {
-        if(leftButton.transform.childCount <= 0 || rightButton.transform.childCount <= 0)
+        if (leftButton.transform.childCount <= 0 || rightButton.transform.childCount <= 0)
         {
             return false;
         }
--- Assets/Scripts/StaticFields.cs
+++ Assets/Scripts/StaticFields.cs
@@ -3,4 +3,7 @@
     public static string VelocityX { get; } = "velocityX";
     public static string Jump { get; } = "jump";
     public static string Slide { get; } = "slide";
+    public static string Mouse { get; } = "Mouse";
+    public static string ToolBar { get; } = "ToolBar";
+    public static string Ready { get; } = "Ready";
 }
